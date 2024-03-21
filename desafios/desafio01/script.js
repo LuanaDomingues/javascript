7ce012594dd7802200a54d52e4f67ffe5d9cc63e
victor matheus dos santos <155080566+LuanaDomingues@users.noreply.github.com>
2024-03-21 04:45:17 -0300

    atualizando exercício

--- desafios/desafio01/script.js
+++ desafios/desafio01/script.js
@@ -1,18 +1,26 @@
 function carregar() {
     var msg = window.document.getElementById('text')
-    var img = window.document.getElementById('imagem')
+    var imag = window.document.getElementById('foto')
+    var mens = window.document.getElementById('tx')
     var data = new Date()
     var hora = data.getHours()
     var min = data.getMinutes()
-    msg.innerHTML = `Agora são ${hora}:${min}`
-    if (hora >= 0 && hora < 12) {
-        img.Image = 'manha.jpg'
+    mens.innerHTML = `Agora são ${hora}:${min}`
+    if (hora >= 6 && hora < 12) {
+        imag.src = ('imagens/manha.jpg')
         document.body.style.background = '#e2cd9f'
-    }else if (hora >= 12 && hora <=18){
-        img.Image = 'tarde.jpg'
+        msg.innerHTML = `<p><strong>BOM DIA!</strong></p>`
+    }else if (hora >= 12 && hora < 19){
+        imag.src = ('imagens/tarde.jpg')
         document.body.style.background = '#7394BF'
-    }else {
-        img.document.Image = 'noite.jpg'
+        msg.innerHTML = `<p><strong>BOA TARDE!</strong></p>`
+    }else if (hora >= 19 && hora < 23){
+        imag.src = ('imagens/noite.jpg')
         document.body.style.background = '#010326'
+        msg.innerHTML = `<p><strong>BOA NOITE!</strong></p>`
+    }else {
+        imag.src = ('imagens/madrugada.png')
+        document.body.style.background = 'black'
+        msg.innerHTML = `<p><strong>VAI DORMIR!</strong></p>`
     }
 }
