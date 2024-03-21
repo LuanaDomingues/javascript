03d76e1db503050dd8d3e5007e5f80dbdcbe64f2
victor matheus dos santos <155080566+LuanaDomingues@users.noreply.github.com>
2024-03-21 19:51:28 -0300

    concluindo desafio

--- desafios/desafio02/script.js
+++ desafios/desafio02/script.js
@@ -10,24 +10,37 @@ function clicar(){
         var idade = ano - Number(fano.value)
         var gen = ''
         var imag = document.createElement('img')
-        imag.setAttribute('id', 'foto')
         if (sx[0].checked){
             gen = 'Homem'
             if (idade >= 0 && idade <= 5){
-                imag.setAttribute('src', 'imagens/bebeboy.jpg')
+                imag.setAttribute('src', 'imagens/boy.png')
             }else if (idade < 12){
-                imag.setAttribute('src', 'imagens/menino.jpg')
+                imag.setAttribute('src', 'imagens/menino.png')
             }else if (idade < 25){
-                imag.setAttribute('src','imagens/rapaz.jpg')
+                imag.setAttribute('src','imagens/rapaz.png')
             }else if(idade < 50){
-                imag.setAttribute('src', 'imagens/homem')
+                imag.setAttribute('src', 'imagens/homem.png')
             }else{
-                imag.setAttribute('src','imagens/idoso.jpg')
+                imag.setAttribute('src','imagens/idoso.png')
             }
         }else if (sx[1].checked){
             gen = 'Mulher'
+            if (idade >= 0 && idade <= 5){
+                imag.setAttribute('src', 'imagens/girl.png')
+            }else if (idade < 12){
+                imag.setAttribute('src', 'imagens/menina.png')
+            }else if (idade < 25){
+                imag.setAttribute('src','imagens/jovem.png')
+            }else if(idade < 50){
+                imag.setAttribute('src', 'imagens/mulher.png')
+            }else{
+                imag.setAttribute('src','imagens/idosa.png')
         }
-        resul.innerHTML = `Você é um(a) ${gen} com ${idade} anos`
-        resul.appendChild(imag)
     }
+        resul.innerHTML = `<strong>Você é um(a) ${gen} com ${idade} anos</strong>`
+        resul.appendChild(imag)
+        imag.style.width = '300px'
+        imag.style.height = '300px'
+        imag.style.marginTop = '20px'
+}
 }
